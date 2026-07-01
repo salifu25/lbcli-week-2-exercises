06c6d1132809bf4fb5c0800d2d26877af03abec6
Salifu Yakubu <salifuyakubu333@gmail.com>
2026-07-01 18:08:32 +0000

    Update 03.sh

--- submission/03.sh
+++ submission/03.sh
@@ -2,13 +2,5 @@
 # Add funds to the address.
 # Return only the Address
 segaddr=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" "bech32")
-
-mineaddr=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress)
-
-bitcoin-cli -regtest generatetoaddress 101 "$mineaddr" >/dev/null
-
-bitcoin-cli -regtest -rpcwallet=btrustwallet sendtoaddress "$segaddr" 1 >/dev/null
-
-bitcoin-cli -regtest generatetoaddress 1 "$mineaddr" >/dev/null
-
+bitcoin-cli -regtest generatetoaddress 1 "$segaddr" > /dev/null
 echo "$segaddr"
